ef03db3d228e2cc9d0071b611b655be85deac6f4
CiceroAON <augustocicero831@gmail.com>
2024-09-21 14:55:17 -0300

    Initial Structure

---
+++ backend/src/main/java/com/backend/saya/SayaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.saya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/saya/SayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.saya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
